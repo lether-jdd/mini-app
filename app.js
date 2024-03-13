4e96b068b100d830c4f112a138dab58275b9034d
lether <jiangdongdong@kuaishou.com>
2024-03-14 00:07:56 +0800

    fix:fix

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ App({
   onLaunch: function () {
     console.log('App Launch')
     wx.login({
-      success: function(loginRes) {
+      success: (loginRes)=> {
+        console.log(1111,{loginRes})
           if (loginRes.code) {
               // example: 081LXytJ1Xq1Y40sg3uJ1FWntJ1LXyth
+              this.getSessionKey(loginRes.code,'wxf8ffa23d7b1ce057',"f157c61f3d6d8584b69992bc8d5f7c32")
           }
       }
     });
@@ -29,16 +31,23 @@ App({
             grant_type: 'authorization_code'
         }
     };
-    return http(opt).then(function (response) {
-        var data = response.data;
-        if (!data.openid || !data.session_key || data.errcode) {
-            return {
-                result: -2,
-                errmsg: data.errmsg || '返回数据字段不完整'
-            }
-        } else {
-            return data
-        }
-    });
+    wx.request({
+      url: opt.url, //仅为示例，并非真实的接口地址
+      data: opt.params,
+      success (res) {
+        console.log(res.data)
+      }
+    })
+    // return http(opt).then(function (response) {
+    //     var data = response.data;
+    //     if (!data.openid || !data.session_key || data.errcode) {
+    //         return {
+    //             result: -2,
+    //             errmsg: data.errmsg || '返回数据字段不完整'
+    //         }
+    //     } else {
+    //         return data
+    //     }
+    // });
 }
 })
